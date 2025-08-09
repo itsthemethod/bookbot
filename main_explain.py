5a7f737b7ddcfbcef4f7127c4fa1df2277edc0f0
itsthemethod <itsthemethod55@gmail.com>
2025-08-09 13:18:34 +1200

    ignoring the books directory, updating man to count words and characters

---
+++ main_explain.py
@@ -0,0 +1,9 @@
+# opening up a new function. with the name get_book_text, and the file path to what you're calling (books) - which is where you store the books for the program to read
+# with is used to open a file in the directory you want. as f gives it a name to call upon it
+# this gives a name to and allows your function to read the test of the files in your set directory
+# return it so that the next part of the program can use it
+
+# define a new function called main which will
+# print the text of the book you want - the exact file path an book
+
+# this calls your main funtion, so that main.py when run will actually run thru the whole code and print the book text
