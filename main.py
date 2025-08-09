ae4f5914f39d8ce12a2cf53ef36a9d54b5361568
itsthemethod <itsthemethod55@gmail.com>
2025-08-09 20:43:14 +1200

    updating spelling

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
 
     import sys
     if len(sys.argv) != 2:
-        print("Usage python3 main.py <path_to_book>")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     book_path = sys.argv[1]
 
